378527b59997ba212b878f5c2104f963baecbd20
rogercodes1 <rogerperez.us@gmail.com>
2018-11-01 08:30:58 -0700

    trying another way to create app. added express.

--- server.js
+++ server.js
@@ -1,57 +1,24 @@
-import sqlite from 'sqlite';
-import Promise from 'bluebird';
-import express from 'express';
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port);
 
-// database promises
-
-const dbPromise = Promise.resolve()
-  .then(() => sqlite.open('./database.sqlite', { Promise }))
-  .then(db => db.migrate({ force: 'last' }));
-
-// API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-if (process.env.NODE_ENV === 'production') {
-  db.migrate() //database migration testing
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
-// const sqlite3 = require('sqlite3').verbose();
-//
-// // open database in memory
-// let db = new sqlite3.Database(':memory:', (err) => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log('Connected to the in-memory SQlite database.');
-// });
-//
-// // close the database connection
-// db.close((err) => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log('Close the database connection.');
-// });
+console.log('App is listening on port ' + port);
