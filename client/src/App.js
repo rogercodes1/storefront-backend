adcdbc7248a652227349974156db547578527fc9
rogercodes1 <rogerperez.us@gmail.com>
2018-11-01 09:42:04 -0700

    working basic app.

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,15 @@ import List from './pages/List';
 
 class App extends Component {
   render() {
-    const App = () => (
-     <div>
-       <Switch>
-         <Route exact path='/' component={Home}/>
-         <Route path='/list' component={List}/>
-       </Switch>
-     </div>
-   )
+  
     return (
-      <Switch>
-        <App/>
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/list' component={List}/>
+        </Switch>
+      </div>
+
     );
   }
 }
