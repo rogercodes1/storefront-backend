41b0d1a522109980e5172bff30b63477446bc9e9
rogercodes1 <rogerperez.us@gmail.com>
2018-10-31 17:11:36 -0700

    added server info.

--- server.js
+++ server.js
@@ -1,11 +1,57 @@
+import sqlite from 'sqlite';
+import Promise from 'bluebird';
+import express from 'express';
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-       res.send('Hello Dev!');
-   });
+// database promises
 
-// Listen to port 5000
-app.listen(5000, function () {
-   console.log('Dev app listening on port 5000!');
-   });
+const dbPromise = Promise.resolve()
+  .then(() => sqlite.open('./database.sqlite', { Promise }))
+  .then(db => db.migrate({ force: 'last' }));
+
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+if (process.env.NODE_ENV === 'production') {
+  db.migrate() //database migration testing
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+// const sqlite3 = require('sqlite3').verbose();
+//
+// // open database in memory
+// let db = new sqlite3.Database(':memory:', (err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Connected to the in-memory SQlite database.');
+// });
+//
+// // close the database connection
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+// });
